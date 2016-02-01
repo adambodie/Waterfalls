6f3682ebd3aabd66a7c1a7cd038175902e35b177
Adam Bodie <aduo3000@yahoo.com>
2016-01-31 17:04:30 -0800

    Updated views and stylings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131184253) do
+ActiveRecord::Schema.define(version: 20160131234139) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160131184253) do
     t.string   "location"
     t.string   "state"
     t.boolean  "visit"
+    t.string   "avatar"
   end
 
 end
